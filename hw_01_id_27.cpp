2c5d5a012917c36269e1d4477900f95751d6f58e
JuanTest <upci.pe1501@gmail.com>
2015-02-10 23:22:31 -0600

    Solved
    the assignment  is ready to review

--- hw_01_id_27.cpp
+++ hw_01_id_27.cpp
@@ -1,10 +1,10 @@
-// Author:
-// Date:
-// Version:
+// Author: Juan Pueblo
+// Date:    today
+// Version:  1.0
 
 /*
-Homework DD
-this assignment  ...
+Homework 01
+this assignment compute the mean for 3 vars
 */
 
 #include <iostream>
@@ -13,7 +13,13 @@ int main ()
 {
   // TODO your code here!!!!
 
-  std::cout<<"Hello world!!" <<std::endl;
+    int x=1,y=2,z=3;
+
+  std::cout<<"x: "<< x<<std::endl;
+  std::cout<<"y: "<< y<<std::endl;
+  std::cout<<"z: "<< z<<std::endl;
+  
+  std::cout<<"mean: (x+y+z)/3  = "<< (x+y+z)/3 <<std::endl;
 
   return 0;
 }
